d3a4e950e162241fbaa98ba53cbea26aff14b37f
sarudego <sruizdegopegui@gmail.com>
2018-01-05 21:55:07 +0100

    preparing web

--- app/run.py
+++ app/run.py
@@ -38,7 +38,7 @@ def home():
 def skills():
     return render_template(
             'skills.html'
-            )
+						)
 
 
 @app.route('/extra')
@@ -48,33 +48,38 @@ def extra():
             )
 
 
-@app.route('/about')
-@app.route('/about/<int:num1>/<int:num2>')
-def about(num1=0, num2=0):
-    contexto = {'num1': num1, 'num2': num2}
-    return render_template(
-            'about.html',
-            **contexto
-            )
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    comment_form = forms.CommentForm(request.form)
-    if request.method == 'POST' and comment_form.validate():
-        print(comment_form.username.data)
-        print(comment_form.email.data)
-        print(comment_form.comment.data)
-    else:
-        print("Not in POST")
+# @app.route('/about')
+# @app.route('/about/<int:num1>/<int:num2>')
+# def about(num1=0, num2=0):
+    # contexto = {'num1': num1, 'num2': num2}
+    # return render_template(
+            # 'about.html',
+            # **contexto
+            # )
 
-    return render_template(
-            'contact.html',
-            email="sruizdegopegui[at]gmail[dot]com",
-            telefono="660 366 599",
-            github="sarudego",
-            form=comment_form
-            )
+
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact():
+    # comment_form = forms.CommentForm(request.form)
+    # if request.method == 'POST' and comment_form.validate():
+        # print(comment_form.username.data)
+        # print(comment_form.email.data)
+        # print(comment_form.comment.data)
+    # else:
+        # print("Not in POST")
+
+    # return render_template(
+            # 'contact.html',
+            # email="sruizdegopegui[at]gmail[dot]com",
+            # telefono="660 366 599",
+            # github="sarudego",
+            # form=comment_form
+            # )
 
 
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -20,6 +20,11 @@ def init():
         'init.html'
     )
 
+@app.route('/index')
+def index():
+    return render_template( 'index.html')
+
+
 
 @app.route('/home')
 def home():
@@ -28,45 +33,23 @@ def home():
     )
 
 
-@app.route('/about')
-@app.route('/about/<int:num1>/<int:num2>')
-def about(num1=0, num2=0):
-    contexto = {'num1': num1, 'num2': num2}
-    return render_template(
-        'about.html',
-        **contexto
-    )
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact():
+    # comment_form = forms.CommentForm(request.form)
+    # if request.method == 'POST' and comment_form.validate():
+        # print(comment_form.username.data)
+        # print(comment_form.email.data)
+        # print(comment_form.comment.data)
+    # else:
+        # print("Not in POST")
 
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    comment_form = forms.CommentForm(request.form)
-    if request.method == 'POST' and comment_form.validate():
-        print(comment_form.username.data)
-        print(comment_form.email.data)
-        print(comment_form.comment.data)
-    else:
-        print("Not in POST")
-
-    return render_template(
-        'contact.html',
-        email="sruizdegopegui[at]gmail[dot]com",
-        telefono="660 366 599",
-        github="sarudego",
-        form=comment_form
-    )
-
-
-@app.route('/skills')
-def skills():
-    return render_template(
-        'skills.html',
-        skill=[
-            {"programming": "Python, AngularJS"},
-            {"languages": "spanish, english"}
-        ]
-
-    )
+    # return render_template(
+        # 'contact.html',
+        # email="sruizdegopegui[at]gmail[dot]com",
+        # telefono="660 366 599",
+        # github="sarudego",
+        # form=comment_form
+    # )
 
 
 @app.route('/extra')
@@ -75,6 +58,11 @@ def extra():
         'extra.html'
     )
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(port=8000)
+
